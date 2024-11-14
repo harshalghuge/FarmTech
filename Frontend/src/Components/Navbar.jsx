0c840e30c77bd29d9fdbbd8b63794641edfb857a
Vishwajeet Ranaware <vishwajeetranaware105@gmail.com>
2024-11-14 11:51:10 +0530

    hi

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Nforjefoirfhiudhiubfibcsbsbbjksnf</div>;
+  return (
+    <div>Nforjefoirnjslcihclshlicjlsjdldcnksdncfhiudhiubfibcsbsbbjksnf</div>
+  );
 }
